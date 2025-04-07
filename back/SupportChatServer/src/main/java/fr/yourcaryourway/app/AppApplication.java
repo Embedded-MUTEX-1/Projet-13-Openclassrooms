bfdcab135d487cca0a9a016f207518b37347f4aa
lenny1411 <105748537+lenny1411@users.noreply.github.com>
2025-04-07 21:48:57 +0200

    Revert "First commit"
    This reverts commit 30f97a11ec7e781966019575c8c57820160afb2d.

--- back/SupportChatServer/src/main/java/fr/yourcaryourway/app/AppApplication.java
+++
@@ -1,9 +0,0 @@
-package fr.yourcaryourway.app;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class AppApplication {
-    public static void main(String[] args) { SpringApplication.run(AppApplication.class, args); }
-}
